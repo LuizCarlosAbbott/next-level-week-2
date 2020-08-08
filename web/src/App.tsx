446f822fcf1c511b6f0d1778e0a51d64639d0e1c
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-08-08 19:12:09 -0300

    feat: 🎸 Creating routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import Landing from './pages/Landing/index'
-import './assets/styles/global.css'
+import React from "react";
+import "./assets/styles/global.css";
+import Routes from "./routes";
 
 function App() {
-  return (
-    <Landing/>
-  );
+  return <Routes />;
 }
 
 export default App;
