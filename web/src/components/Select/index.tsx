edb4119d4ad179d551956f55ce1a7733a79ed2cd
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-08-09 17:24:09 -0300

    feat: 🎸 Preparando cadastro de horarios

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -16,7 +16,7 @@ const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
       <select id={name} {...rest}>
-        <option value="" disabled selected hidden>
+        <option value="" disabled hidden defaultValue="">
           Selecione uma opção
         </option>
         {options.map((option) => {
