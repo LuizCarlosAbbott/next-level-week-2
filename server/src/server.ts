37139c2c22ab977d83e2a3824bcddca03470ffd9
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-08-09 11:09:42 -0300

    feat: 🎸 Database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  const users = [
-    { name: "Diego", age: 25 },
-    { name: "Vini", age: 25 },
-  ];
+app.use(express.json());
 
-  return response.json(users);
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
 });
 
 app.listen(3333);
