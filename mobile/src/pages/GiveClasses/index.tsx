a0b5ea53f72e7191e7e29849d9dab49b5632e4c4
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-08-09 20:48:48 -0300

    feat: 🎸 Give Classes page

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,9 +1,37 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ImageBackground, Text } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+
+import giveClassesBgImage from "../../assets/images/give-classes-background.png";
+
+import styles from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
 function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
   return (
-    <View/>
+    <View style={styles.container}>
+      <ImageBackground
+        source={giveClassesBgImage}
+        style={styles.content}
+        resizeMode="contain"
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, primeiro você precisa se cadastrar como professor na
+          nossa plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo Bem</Text>
+      </RectButton>
+    </View>
   );
 }
 
