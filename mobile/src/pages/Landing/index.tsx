b9e64443382a879524c623470ff8691014425573
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-08-09 22:43:24 -0300

    feat: 🎸 Api connection

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Image, Text, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Image, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
 import styles from "./styles";
@@ -8,8 +8,19 @@ import landingImg from "../../assets/images/landing.png";
 import studyIcon from "../../assets/images/icons/study.png";
 import giveClassesIcon from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
+import api from "../../services/api";
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("connections").then((response) => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
   const { navigate } = useNavigation();
 
   function handleNavigateToGiveClassesPage() {
@@ -50,7 +61,8 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 9 de conexões realizadas <Image source={heartIcon} />
+        Total de {totalConnections} de conexões realizadas{" "}
+        <Image source={heartIcon} />
       </Text>
     </View>
   );
