b9a572923168af1c9e07ca3ceac7ec0fb1eac4af
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-08-09 21:18:33 -0300

    perf: ⚡️ Page Header

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { View } from "react-native";
 import styles from "./styles";
+import PageHeader from "../components/PageHeader";
 
 function TeacherList() {
-  return <View style={styles.container} />;
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Proffys disponiveis" />
+    </View>
+  );
 }
 
 export default TeacherList;
