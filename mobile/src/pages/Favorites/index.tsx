4afa3e97f1e12332b5bb46a804023695bdfc0b42
LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-08-09 23:35:19 -0300

    feat: 🎸 favorited list

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, ScrollView } from "react-native";
 import styles from "./styles";
 import PageHeader from "../components/PageHeader";
-import TeacherItem from "../components/TeacherItem";
+import TeacherItem, { Teacher } from "../components/TeacherItem";
+import AsyncStorage from "@react-native-community/async-storage";
 
 function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem("favorites").then((response) => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+
+        setFavorites(favoritedTeachers);
+      }
+    });
+  }
+
+  useEffect(() => {
+    loadFavorites();
+  }, []);
+
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys favoritos" />
@@ -16,12 +33,9 @@ function Favorites() {
           paddingBottom: 16,
         }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {favorites.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} favorited />;
+        })}
       </ScrollView>
     </View>
   );
